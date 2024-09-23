c88f9d2d80819aa95ccef9c7ec797a34363596c0
laurebouet <93431071+laurebouet@users.noreply.github.com>
2024-09-23 17:05:16 +0200

    creating responsiveness

---
+++ JavaScript/menuicon.js
@@ -0,0 +1,15 @@
+function display(){
+    var menu = document.getElementsByClassName(menu);
+    console.log(menu.length);
+    console.log("oui");
+    // console.log(menu);
+    // document.getElementsByTagName("main").style.display = "none";
+    if(menu.style.display == "none"){
+            // menu.style.display = "inline";
+            console.log("menu is displayed");
+        }
+}
+
+// function hide(){
+
+// }
