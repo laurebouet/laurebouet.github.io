248d7bad8a7a096f2ad199a45724ba7b5d013010
laurebouet <93431071+laurebouet@users.noreply.github.com>
2024-10-09 16:15:38 +0200

    alert message, footer

---
+++ JavaScript/alertmessage.js
@@ -0,0 +1,18 @@
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-danger" role="alert">`,
+    `   <div>${message}</div>`,
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('This service isn\'t available yet!', 'success')
+  })
+}
