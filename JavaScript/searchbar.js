bb9f2e3a8513b00d37c8e64292a624e0b85786d2
laurebouet <93431071+laurebouet@users.noreply.github.com>
2024-10-09 11:34:42 +0200

    search bar alt works and css

--- JavaScript/searchbar.js
+++ JavaScript/searchbar.js
@@ -1,36 +1,27 @@
 //bar de recherche
 
 function searchbar(){
-    var input, filter, altText, img, i, objets,objet, count;
+    var input, filter, altText, img, i, objets, count;
     count=0;
     input =  document.getElementById("recherche");
     filter = input.value.toUpperCase();
     objets = document.getElementsByClassName("objet");
-    // console.log(objets.length);
 
-    for(i=0;i<objets.length+1;i++){
+    for(i=0;i<objets.length;i++){
         
-        objet = document.getElementsByClassName("imageObjet");
-
-        // objet = document.querySelector(".imageObjet");
-        // console.log(objets[i]);
-        // altText=objet.alt;
-        // console.log(altText);
-        // console.log(objet);
-
+        // objet = document.getElementsByClassName("imageObjet");
         img= document.getElementsByClassName("imageObjet")[i];
         altText = img.alt;
         
         if(altText.toUpperCase().indexOf(filter) > -1){
-            objets[i].style.display = "";
-            // objet.style.display = "";
             console.log(img);
             console.log(altText);
+
+            objets[i].style.display = "";
             count=count+1;
         }
         else{
             objets[i].style.display = "none";
-            // objet.style.display = "none";
         }
     }
     document.getElementById("res").innerHTML =count+" résulat(s) trouvé(s)";
